fc5613c43b9b078ea3ee30084a038d8a7da49166
miguelg1903 <miguelg1903@gmail.com>
2018-12-27 10:28:12 -0500

    cambios en texto travel_agent

--- resources/lang/en/cubabien.php
+++ resources/lang/en/cubabien.php
@@ -2,7 +2,7 @@
 
 return [
     'travel-agent.title' => 'Why Cuba Bien Travel?',
-    'travel-agent.p1' => '<b>Commission plan:</b> Base land commission, the base commission on Cuba Bien Travel will be 10% from your first confirmed booking.',
+    'travel-agent.p1' => '<b>Commission plan:</b> the base land commission on Cuba Bien Travel will be 10% from your first confirmed booking.',
     'travel-agent.lista' => '<ul class="link"><li><span><i class="fa fa-check-circle fa-sm"></i></span> Expert in Cuba travel</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> Unique tours</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> Private groups and Independent tour options</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> We make easy for you</li></ul>',
     'travel-agent.p2' => '<b>How to get Started?:</b>',
     'travel-agent.login' => 'Login',
--- resources/lang/es/cubabien.php
+++ resources/lang/es/cubabien.php
@@ -2,7 +2,7 @@
 
 return [
     'travel-agent.title' => '¿Por qué CubaBien Travel?',
-    'travel-agent.p1' => '<b>Plan de Comisión:</b> Comisión base inicial, la comisión base en CubaBien Travel será de un 10% a partir de su primera reserva confirmada.',
+    'travel-agent.p1' => '<b>Plan de Comisión:</b> la comisión base en CubaBien Travel será de un 10% a partir de su primera reserva confirmada.',
     'travel-agent.lista' => '<ul class="link"><li><span><i class="fa fa-check-circle fa-sm"></i></span> Expertos en viajes a Cuba</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> Tours Únicos</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> Grupos privados y opciones independientes en los Tours</li><li><span><i class="fa fa-check-circle fa-sm"></i></span> Hacemos las cosas fáciles para ti</li></ul>',
     'travel-agent.p2' => '<b>¿Cómo empezar?:</b>',
     'travel-agent.login' => 'Iniciar Sesión',
